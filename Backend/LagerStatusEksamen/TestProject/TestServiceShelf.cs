085f50d8c5a0a3ded9e5412ee7da72be63aef230
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-11-28 13:13:41 +0100

    Test for Add and GetAll shelves

--- Backend/LagerStatusEksamen/TestProject/TestServiceShelf.cs
+++ Backend/LagerStatusEksamen/TestProject/TestServiceShelf.cs
@@ -1,3 +1,8 @@
+using LagerStatusEksamen.Interfaces;
+using LagerStatusEksamen.Models;
+using LagerStatusEksamen.Services;
+using Microsoft.AspNetCore.Hosting.Server;
+
 namespace TestProject
 {
     [TestClass]
@@ -6,30 +11,59 @@ namespace TestProject
         [TestMethod]
         public void TestGetAllShelfs()
         {
-            
+            //Arrange
+            IServiceShelf _service = new ServiceShelf();
+            List<Shelf> _allShelf = _service.GetAll();
+
+
+            //Assert
+            Assert.IsNotNull(_allShelf);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void TestFailedGetAllShelfs()
+        {
+            //Arrange
+            IServiceShelf _service = new ServiceShelf();
+            List<Shelf> _allShelf = _service.GetAll();
         }
 
         [TestMethod]
         public void TestAddShelf()
         {
-            
+            //Arrange
+            IServiceShelf _service = new ServiceShelf();
+            List<Shelf> _allShelf = _service.GetAll();
+
+            //Act
+            int countBefore = _allShelf.Count;
+
+            Shelf s = new Shelf("75515f", "Ruber", true);
+            _service.Add(s);
+
+            int counterAfter = _allShelf.Count;
+            _service.Delete(s.MAC);
+
+            //Assert
+            Assert.AreEqual(countBefore + 1, counterAfter);
         }
 
-       [TestMethod]
+        [TestMethod]
         public void TestGetShelfByMAC()
         {
-            
+
         }
 
         [TestMethod]
         public void TestDeleteShelf()
         {
-            
+
         }
-                [TestMethod]
+        [TestMethod]
         public void TestUpdateShelf()
         {
-            
+
         }
     }
 }
