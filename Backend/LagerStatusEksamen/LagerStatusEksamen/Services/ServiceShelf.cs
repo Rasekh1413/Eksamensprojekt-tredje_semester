6a09ec71fd07cd28f32f7970ab2d55d289a0f651
Infinity-MaxX <katerina@oestergaard.com>
2025-12-01 09:50:40 +0100

    Finished ServiceShelf methods. NOT TESTED.

--- Backend/LagerStatusEksamen/LagerStatusEksamen/Services/ServiceShelf.cs
+++ Backend/LagerStatusEksamen/LagerStatusEksamen/Services/ServiceShelf.cs
@@ -1,7 +1,8 @@
 ﻿using LagerStatusEksamen.Interfaces;
 using LagerStatusEksamen.Models;
-using LagerStatusEksamen.Misc;
+using Microsoft.AspNetCore.DataProtection;
 using Microsoft.Data.SqlClient;
+using System;
 
 namespace LagerStatusEksamen.Services
 {
@@ -9,12 +10,14 @@ namespace LagerStatusEksamen.Services
     {
         #region Instances
         private string selectSql = "SELECT * FROM Shelves";
-        private string filterBySensorSql = "SELECT * FROM Shelves WHERE ID = @ID";
-        private string insertSql = "INSERT INTO Shelves(MAC,PackageType,IsStocked) Values(@MAC, @PackageType, @IsStocked)";
-        private string deleteSql = "DELETE FROM Shelves WHERE ID = @ID";
-        private string updateSql = "UPDATE Shelves SET PackageType = @PackageType, IsStocked = @IsStocked WHERE ID = @ID";
+        private string filterBySensorSql = "SELECT * FROM Shelves WHERE MAC = @MAC";
+        private string insertSql = "INSERT INTO Shelves(MAC, PackageType, IsStocked) Values(@MAC, @PackageType, @IsStocked)";
+        private string deleteSql = "DELETE FROM Shelves WHERE MAC = @MAC";
+        private string updatePackageSql = "UPDATE Shelves SET PackageType = @PackageType WHERE MAC = @MAC";
+        private string updateStatusSql = "UPDATE Shelves SET IsStocked = @IsStocked WHERE MAC = @MAC";
         #endregion
 
+        #region Methods
         public Shelf Add(Shelf shelf)
         {
             Shelf addedShelf = new Shelf();
@@ -26,15 +29,11 @@ namespace LagerStatusEksamen.Services
                     command.Parameters.AddWithValue("MAC", shelf.MAC);
                     command.Parameters.AddWithValue("PackageType", shelf.PackageTypeName);
                     command.Parameters.AddWithValue("IsStocked", shelf.IsStocked);
-
                     connection.Open();
 
-                    int numberOfRows = command.ExecuteNonQuery();
+                    command.ExecuteNonQuery();
 
-                    if (numberOfRows > 0)
-                    {
-                        addedShelf = shelf;
-                    }
+                    addedShelf = GetByMAC(shelf.MAC);
                 }
                 catch (SqlException ex)
                 {
@@ -42,13 +41,30 @@ namespace LagerStatusEksamen.Services
                     throw ex;
                 }
                 catch (Exception ex) { throw ex; }
+                finally { connection.Close(); }
                 return addedShelf;
             }
         }
 
         public Shelf? Delete(string mac)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(Secret.ConnectionString))
+            {
+                Shelf? deleteShelf = GetByMAC(mac);
+                if (deleteShelf == null) { return null; }
+                try
+                {
+                    SqlCommand command = new SqlCommand(deleteSql, connection);
+                    command.Parameters.AddWithValue("@MAC", mac);
+                    connection.Open();
+
+                    command.ExecuteNonQuery();
+                }
+                catch (SqlException ex) { throw ex; }
+                catch (Exception ex) { throw ex; }
+                finally { connection.Close(); }
+                return deleteShelf;
+            }
         }
 
         public List<Shelf> GetAll()
@@ -71,26 +87,84 @@ namespace LagerStatusEksamen.Services
                         Shelf shelf = new Shelf(mac, packageType, isStocked);
                         shelves.Add(shelf);
                     }
+                    reader.Close();
                 }
                 catch (SqlException ex) { throw ex; }
                 catch (Exception ex) { throw ex; }
+                finally { connection.Close(); }
                 return shelves;
             }
         }
 
         public Shelf? GetByMAC(string mac)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(Secret.ConnectionString))
+            {
+                Shelf? shelf = new Shelf();
+                if (mac == null) { return null; }
+                try
+                {
+                    SqlCommand command = new SqlCommand(filterBySensorSql, connection);
+                    command.Parameters.AddWithValue("@MAC", mac);
+                    connection.Open();
+                    SqlDataReader reader = command.ExecuteReader();
+
+                    while (reader.Read())
+                    {
+                        string getPackageType = reader.GetString(1);
+                        bool getIsStocked = reader.GetBoolean(2);
+                        shelf = new Shelf(mac, getPackageType, getIsStocked);
+                    }
+                    reader.Close();
+                }
+                catch (SqlException ex) { throw ex; }
+                catch (Exception ex) { throw ex; }
+                finally { connection.Close(); }
+                return shelf;
+            }
         }
 
         public Shelf? UpdatePackageType(string mac, string type)
         {
-            throw new NotImplementedException();
+            using(SqlConnection connection = new SqlConnection(Secret.ConnectionString))
+            {
+                Shelf? shelf = GetByMAC(mac);
+                if (shelf == null) { return null; }
+                try
+                {
+                    SqlCommand command = new SqlCommand(updatePackageSql, connection);
+                    command.Parameters.AddWithValue("@MAC", mac);
+                    command.Parameters.AddWithValue("@PackageType", type);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch(SqlException ex) { throw ex; }
+                catch (Exception ex) { throw ex; }
+                finally { connection.Close(); }
+                return shelf;
+            }
         }
 
         public Shelf? UpdateStatus(string mac, bool status)
         {
-            throw new NotImplementedException();
+            using (SqlConnection connection = new SqlConnection(Secret.ConnectionString))
+            {
+                Shelf? shelf = GetByMAC(mac);
+                if (shelf == null) { return null; }
+                try
+                {
+                    SqlCommand command = new SqlCommand(updateStatusSql, connection);
+                    command.Parameters.AddWithValue("@MAC", mac);
+                    command.Parameters.AddWithValue("@IsStocked", status);
+                    connection.Open();
+                    command.ExecuteNonQuery();
+                }
+                catch (SqlException ex) { throw ex; }
+                catch (Exception ex) { throw ex; }
+                finally { connection.Close(); }
+                return shelf;
+            }
         }
+        #endregion
     }
 }
