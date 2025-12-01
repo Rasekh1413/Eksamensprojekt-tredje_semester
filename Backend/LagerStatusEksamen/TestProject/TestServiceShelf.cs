57b46b95ea902aa88d8211cb12b4215ce13edf94
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-12-01 10:39:51 +0100

    Done MSTest. NOT TESTED.

--- Backend/LagerStatusEksamen/TestProject/TestServiceShelf.cs
+++ Backend/LagerStatusEksamen/TestProject/TestServiceShelf.cs
@@ -20,14 +20,14 @@ namespace TestProject
             Assert.IsNotNull(_allShelf);
         }
 
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void TestFailedGetAllShelfs()
-        {
-            //Arrange
-            IServiceShelf _service = new ServiceShelf();
-            List<Shelf> _allShelf = _service.GetAll();
-        }
+        //[TestMethod]
+        //[ExpectedException(typeof(ArgumentNullException))]
+        //public void TestFailedGetAllShelfs()
+        //{
+        //    //Arrange
+        //    IServiceShelf _service = new ServiceShelf();
+        //    List<Shelf> _allShelf = _service.GetAll();
+        //}
 
         [TestMethod]
         public void TestAddShelf()
@@ -52,18 +52,72 @@ namespace TestProject
         [TestMethod]
         public void TestGetShelfByMAC()
         {
+            //Arrange
+            IServiceShelf _service = new ServiceShelf();
+            List<Shelf> _allShelf = _service.GetAll();
 
+            //Act
+            Shelf sh = new Shelf("8855", "Bracelets", true);
+            _service.Add(sh);
+
+            Shelf? foundShelf = _service.GetByMAC(sh.MAC);
+            _service.Delete(sh.MAC);
+
+            //Assert
+            Assert.AreEqual(sh, foundShelf);
         }
 
         [TestMethod]
         public void TestDeleteShelf()
         {
+            //Arrange
+            IServiceShelf _service = new ServiceShelf();
+            List<Shelf> _allShelf = _service.GetAll();
 
+            //Act
+            Shelf s = new Shelf("0987h", "Napkin", true);
+            _service.Add(s);
+            int counterBefore = _allShelf.Count;
+
+            _service.Delete(s.MAC);
+            int counterAfter = _allShelf.Count;
+
+            //Assert
+            Assert.AreEqual(counterBefore - 1, counterAfter);
         }
         [TestMethod]
-        public void TestUpdateShelf()
+        public void TestUpdateShelfPackageType()
         {
+            //Arrange
+            IServiceShelf _service = new ServiceShelf();
+            //List<Shelf> _allShelf = _service.GetAll();
 
+            //Act
+            Shelf s = new Shelf("977r4", "Napkin", true);
+            string newPTName = "rubber duck";
+
+            Shelf? updatesShelf = _service.UpdatePackageType(s.MAC, newPTName);
+            _service.Delete(s.MAC);
+
+            //Assert
+            Assert.AreEqual(updatesShelf.PackageTypeName, newPTName);
+
+        }
+
+        public void TestUpdateShelfStatus()
+        {
+            //Arrange
+            IServiceShelf _service = new ServiceShelf();
+            //List<Shelf> _allShelf = _service.GetAll();
+
+            //Act
+            Shelf s = new Shelf("ofg876", "Napkin", false);
+            bool newStatus = true;
+
+            Shelf? updatesShelf = _service.UpdateStatus(s.MAC, newStatus);
+
+            //Assert
+            Assert.AreEqual(updatesShelf.IsStocked, newStatus);
         }
     }
 }
