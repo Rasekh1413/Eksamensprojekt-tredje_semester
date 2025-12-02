29510ef1a8bacc5d8020f4fbdd205f97dceead1a
Johanne Marie Dehn Nielsen <jon010@edu.zealand.dk>
2025-12-02 14:02:22 +0100

    Klargjotdt til implementering af rest service og axious

--- Browser/JS/main.js
+++ Browser/JS/main.js
@@ -1,11 +1,54 @@
+//PT will be a reference to 'PackageType'
 
+const baseURl =''
 const app = Vue.createApp({
     data() {
         return {
-            showHyldeList:[{ID:1,Status:true,PackageTypeName:'Duftlys',MAC:'098556'},{ID:2,Status:true,PackageTypeName:'BodyScup',MAC:'9649h'}],
-            hylde:'',
+            shelfInDB:[],
+            showShelfList:[{ID:1,Status:true,PackageTypeName:'Duftlys',MAC:'098556'},{ID:2,Status:true,PackageTypeName:'BodyScup',MAC:'9649h'}],
+            shelf:'',
+            PTInDB:[],
+            showPTList:[],
+            PT:[]
+
+
         }
     },
     methods: {
+        //Methods using shelf
+        getAllShelf(){
+
+        },
+        deleteShelf(){
+
+        },
+        UpdateShelfPackagtype(){
+
+        },
+        UpdateShelfStatus(){
+
+        },
+        //Methods usign Packagetype
+        getAllPT(){
+
+        },
+        addPT(){
+
+        },
+        deletePT(){
+
+        },
+        updatePT(){
+
+        },
+        getPTByName(){
+
+        }
+        ,
+        //Methods for filthering
+
+        //Methods for sorting
+
+
     }
 })
