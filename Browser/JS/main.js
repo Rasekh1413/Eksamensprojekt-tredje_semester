59705c566f876ce86109bca1a8738a3ae037aac1
Alexpoelse1 <alexpoelsemart@gmail.com>
2025-12-05 11:45:10 +0100

    live updates now work

--- Browser/JS/main.js
+++ Browser/JS/main.js
@@ -1,171 +1,134 @@
-//PT will be a reference to 'PackageType'
-const baseURL='http://localhost:5155/api/'
-const baseURLShelf =baseURL+'Shelf/'
-const baseURLPt=baseURL+'PackageType/'
+// PT will be a reference to 'PackageType'
+const baseURL = 'http://localhost:5155/api/';
+const baseURLShelf = baseURL + 'Shelf/';
+const baseURLPt = baseURL + 'PackageType/';
 
 const app = Vue.createApp({
     data() {
         return {
-            shelfInDB:[],
-            showShelfList:[],
-            shelf:'',
-            newptName:'',
-            PTInDB:[],
-            showPTList:[],
-            PT:[],
-            ptNewDescription:[],
-            ptNewName:[]
-
-
+            shelfInDB: [],
+            showShelfList: [],
+            shelf: '',
+            newptName: '',
+            PTInDB: [],
+            showPTList: [],
+            PT: [],
+            ptNewDescription: [],
+            ptNewName: []
         }
     },
+
     methods: {
-        //Methods using shelf
-        getAllShelf(){
-            console.log("Er i metoden GetAllShelf")
+        // Methods using shelf
+        getAllShelf() {
+            console.log("Er i metoden GetAllShelf");
             axios.get(baseURLShelf)
-            .then(
-                response=>{
-                    console.log(response.data)
-                    this.shelfInDB = response.data
-                    this.showShelfList= this.shelfInDB
-                }
-            ).catch(
-                error=>{
-                    console.log(error)
-                }
-            )
-            console.log("Færdig i metoden GetAllShelf")
-        },
-        deleteShelf(mac){
-            console.log("Er i metoden DeleteShelf")
-            axios.delete(baseURLShelf+mac)
-            .then(
-                response =>{
-                    console.log(response)
-                }
-            ).catch(
-                error=>{
-                    console.log(error)
-                }
-            )
-            this.getAllShelf()
-        },
-        UpdateShelfPackagtype(mac){
-            console.log("Er i metoden UpdateShelf")
-            console.log(this.newptName)
-            axios.put(baseURLShelf+mac+'/'+this.newptName)
-            .then(
-                response =>{
-                    console.log(response)
-                }
-            ).catch(
-                error=>{
-                    console.log(error)
-                }
-            )
-            this.getAllShelf()
-        },
-        getShelfFromMac(mac){
-            console.log("Er i metoden GetShelfFromMac")
-            axios.get(baseURLShelf+mac)
-            .then(
-                response=>{
-                    console.log(response.data)
-                    this.shelf = response.data
-                }
-            ).catch(
-                error=>{
-                    console.log(error)
-                }
-            )
-            console.log("Færdig i metoden GetAllShelf")
+            .then(response => {
+                console.log(response.data);
+                this.shelfInDB = response.data;
+                this.showShelfList = this.shelfInDB;
+            })
+            .catch(error => {
+                console.log(error);
+            });
         },
 
-        //Methods usign Packagetype
-        getAllPT(){
-            console.log("Er i metoden GetAllPt")
+        deleteShelf(mac) {
+            console.log("Er i metoden DeleteShelf");
+            axios.delete(baseURLShelf + mac)
+            .then(response => { console.log(response); })
+            .catch(error => { console.log(error); });
+            this.getAllShelf();
+        },
+
+        UpdateShelfPackagtype(mac) {
+            console.log("Er i metoden UpdateShelf");
+            console.log(this.newptName);
+            axios.put(baseURLShelf + mac + '/' + this.newptName)
+            .then(response => { console.log(response); })
+            .catch(error => { console.log(error); });
+            this.getAllShelf();
+        },
+
+        getShelfFromMac(mac) {
+            console.log("Er i metoden GetShelfFromMac");
+            axios.get(baseURLShelf + mac)
+            .then(response => {
+                console.log(response.data);
+                this.shelf = response.data;
+            })
+            .catch(error => { console.log(error); });
+        },
+
+        // Methods using PackageType
+        getAllPT() {
+            console.log("Er i metoden GetAllPt");
             axios.get(baseURLPt)
-            .then(
-                response=>{
-                    console.log(response.data)
-                    this.PTInDB = response.data
-                    this.showPTList= this.PTInDB
-                }
-            ).catch(
-                error=>{
-                    console.log(error)
-                }
-            )
-            console.log("Færdig i metoden GetAllPt")
+            .then(response => {
+                console.log(response.data);
+                this.PTInDB = response.data;
+                this.showPTList = this.PTInDB;
+            })
+            .catch(error => { console.log(error); });
+        },
 
+        addPT() {
+            console.log("Er i metoden addPT");
+            axios.post(baseURLPt, { "name": this.ptNewName, "description": this.ptNewDescription })
+            .then(response => { console.log(response); })
+            .catch(error => { console.log(error); });
+            this.getAllPT();
         },
-        addPT(){
-            console.log("Er i metoden addPT")
-            axios.post(baseURLPt,{"name":this.ptNewName,"description":this.ptNewDescription})
-            .then( response =>{
-                console.log(response)
-            }).catch(
-                error=>{
-                    console.log(error)
-                }
-            )
-            console.log("Færdig i metoden addPT")
-            this.getAllPT()
-        },
-        deletePT(name){
-            console.log("Er i metoden deletePT")
-            axios.delete(baseURLPt+name).then(
-                response=>{
-                    console.log(response)
-                }
-            ).catch(
-                error=>{
-                    console.log(error)
-                }
-            )
-            console.log("Færdig i metoden deletePT")
-            this.getAllPT()
-        },
-        updatePT(){
-            console.log("Er i metoden UpdatePT")
-            console.log(this.ptNewDescription)
-            axios.put("http://localhost:5155/api/PackageType",{"name":this.PT.name,"description":this.ptNewDescription})
-            .then(
-                response =>{
-                    console.log(response)
-                }
-            ).catch(
-                error=>{
-                    console.log(error)
-                }
-            )
-            this.getAllPT()
 
+        deletePT(name) {
+            console.log("Er i metoden deletePT");
+            axios.delete(baseURLPt + name)
+            .then(response => { console.log(response); })
+            .catch(error => { console.log(error); });
+            this.getAllPT();
         },
-        getPTByName(name){
-            console.log("Er i metoden GetPtByName")
-            console.log(name)
-            axios.get(baseURLPt+name)
-            .then(
-                response=>{
-                    console.log(response.data)
-                    this.PT = response.data
-                    this.ptNewDescription=this.PT.description
-                }
-            ).catch(
-                error=>{
-                    console.log(error)
-                }
-            )
-            console.log(this.ptNewDescription)
-            console.log("Færdig i metoden GetPtByName")
+
+        updatePT() {
+            console.log("Er i metoden UpdatePT");
+            axios.put(baseURLPt, { "name": this.PT.name, "description": this.ptNewDescription })
+            .then(response => { console.log(response); })
+            .catch(error => { console.log(error); });
+            this.getAllPT();
+        },
+
+        getPTByName(name) {
+            console.log("Er i metoden GetPtByName");
+            axios.get(baseURLPt + name)
+            .then(response => {
+                console.log(response.data);
+                this.PT = response.data;
+                this.ptNewDescription = this.PT.description;
+            })
+            .catch(error => { console.log(error); });
         }
-        ,
-        //Methods for filthering
+    },
 
-        //Methods for sorting
+    // ✅ Auto-load on page load + auto-refresh every 10 seconds
+    mounted() {
+        // Load immediately when the page loads
+        this.getAllShelf();
+        this.getAllPT();
 
+        // Refresh shelves every 10 seconds
+        this.shelfTimer = setInterval(() => {
+            this.getAllShelf();
+        }, 10000);
 
+        // Refresh package types every 10 seconds
+        this.ptTimer = setInterval(() => {
+            this.getAllPT();
+        }, 10000);
+    },
+
+    beforeUnmount() {
+        clearInterval(this.shelfTimer);
+        clearInterval(this.ptTimer);
     }
-})
+});
+
+app.mount('#app');
