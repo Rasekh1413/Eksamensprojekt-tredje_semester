83081f80d1620268fe7860d18ddb9ac6b5f4321a
Rasekh Tasharoki <rasekh1413@icloud.com>
2025-12-11 10:24:09 +0100

    Final version 0.1

---
+++ Backend/LagerStatusEksamen/LagerStatusEksamen/Misc/Secret.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace LagerStatusEksamen
+{
+    public static class Secret
+    {
+        public static string ConnectionString { get; }
+
+        static Secret()
+        {
+            var builder = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            IConfiguration config = builder.Build();
+            ConnectionString = config.GetConnectionString("ServerDB");
+        }
+    }
+}
+
+
+//namespace LagerStatusEksamen
+//{
+//    public class Secret
+//    {
+//        public static string ConnectionString { get { return _connectionString; } }
+//        private static string _connectionString = @"Data Source=(localdb)\\MSSQLLocalDB;Initial Catalog=LagerStatusDB;Integrated Security=True;Connect Timeout=30;Encrypt=False;Trust Server Certificate=False;Application Intent=ReadWrite;Multi Subnet Failover=False";
+//    }
+//}
